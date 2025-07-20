f297a6f8d211ee484c8caad3870fa1c8e150ffee
Phyzait Lu <i@phyzait.moe>
2025-07-20 17:01:36 +0800

    add

--- flake.nix
+++ flake.nix
@@ -12,20 +12,31 @@
         pkgs = import nixpkgs { inherit system; };
         fhsEnv = pkgs.buildFHSUserEnv {
           name = "fhs-env";
-          targetPkgs = pkgs: with pkgs; [
-            nodejs_20
-          ];
+          targetPkgs = pkgs:
+            with pkgs;
+            [
+              nodejs_20
+
+              libxkbcommon # libxkbcommon.so
+              at-spi2-core # libatspi.so
+              mesa # libdbm.so libexpat.so
+              cairo # libcairo.so
+              cups # libcups.so
+              gtk3 # libgtk-3.so
+              pango # libpango-1.0.so
+              nss # libnss3.so
+              alsa-lib # libasound.so
+              glib # libglib-2.0.so libgobject-2.0.so libgio.so
+              stdenv.cc.cc.lib # libgcc_s.so
+            ] ++ (with xorg; [
+              libX11 # libX11.so
+              libXcomposite # libXcomposite.so
+              libXdamage # libXdamage.so
+              libXext # libXext.so
+              libXfixes # libXfixes.so
+              libXrandr # libXrandr.so
+            ]);
           runScript = "bash";
         };
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              nodejs_20
-            ];
-          };
-          fhs = fhsEnv.env;
-        };
-      }
-    );
+      in { devShells = { default = fhsEnv.env; }; });
 }
