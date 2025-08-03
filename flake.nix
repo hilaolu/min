171a592741550eac5a3b05dd4228864caad7d3f6
Phyzait Lu <i@phyzait.moe>
2025-08-03 22:17:22 +0800

    update flake.nix and add package.nix

--- flake.nix
+++ flake.nix
@@ -10,38 +10,13 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        fhsEnv = pkgs.buildFHSUserEnv {
-          name = "fhs-env";
-          targetPkgs = pkgs:
-            with pkgs;
-            [
-              nodejs_20
-
-              libxkbcommon # libxkbcommon.so
-              at-spi2-core # libatspi.so
-              mesa # libdbm.so libexpat.so
-              cairo # libcairo.so
-              cups # libcups.so
-              gtk3 # libgtk-3.so
-              pango # libpango-1.0.so
-              nss # libnss3.so
-              alsa-lib # libasound.so
-              glib # libglib-2.0.so libgobject-2.0.so libgio.so
-              stdenv.cc.cc.lib # libgcc_s.so
-              udev
-              nspr
-              dbus
-              expat
-            ] ++ (with xorg; [
-              libX11 # libX11.so
-              libXcomposite # libXcomposite.so
-              libXdamage # libXdamage.so
-              libXext # libXext.so
-              libXfixes # libXfixes.so
-              libXrandr # libXrandr.so
-              libxcb
-            ]);
-          runScript = "bash";
+        minBrowser = pkgs.callPackage ./package.nix { };
+      in {
+        packages = {
+          default = minBrowser;
+          min = minBrowser;
         };
-      in { devShells = { default = fhsEnv.env; }; });
+        devShells.default =
+          pkgs.mkShell { buildInputs = with pkgs; [ nodejs_20 electron ]; };
+      });
 }
