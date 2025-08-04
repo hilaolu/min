9141437b50b71c9487aa8102a3a3e47dd5c04f01
Phyzait Lu <i@phyzait.moe>
2025-08-04 12:34:06 +0800

    switch to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment with Node.js";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
